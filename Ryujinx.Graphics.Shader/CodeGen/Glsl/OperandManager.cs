c4db8f69c1bc616edde23f51c77613ef5f51f98c
jduncanator <jduncanator@hotmail.com>
2019-10-31 21:04:28 +1100

    Implement gl_ViewportIndex Vertex Attribute

--- Ryujinx.Graphics.Shader/CodeGen/Glsl/OperandManager.cs
+++ Ryujinx.Graphics.Shader/CodeGen/Glsl/OperandManager.cs
@@ -29,6 +29,7 @@ namespace Ryujinx.Graphics.Shader.CodeGen.Glsl
                    new Dictionary<int, BuiltInAttribute>()
         {
             { AttributeConsts.Layer,               new BuiltInAttribute("gl_Layer",           VariableType.S32)  },
+            { AttributeConsts.ViewportIdx,         new BuiltInAttribute("gl_ViewportIndex",   VariableType.S32)  },
             { AttributeConsts.PointSize,           new BuiltInAttribute("gl_PointSize",       VariableType.F32)  },
             { AttributeConsts.PositionX,           new BuiltInAttribute("gl_Position.x",      VariableType.F32)  },
             { AttributeConsts.PositionY,           new BuiltInAttribute("gl_Position.y",      VariableType.F32)  },
